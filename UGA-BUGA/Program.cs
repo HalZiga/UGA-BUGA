c596e71b1f3e43585258bc3adc18ef7c2769890a
zigan <zigan@LAPTOP-VRFQNEJH>
2022-09-16 07:49:17 +0300

    Добавьте файлы проекта.

---
+++ UGA-BUGA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Приветствую тебя {name}");
+            Console.ReadKey();
+        }
+    }
+}
